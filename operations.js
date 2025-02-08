5f7fdca24823732171503775955a9944b2f2c591
Koustav Biswas <bkoustav361@gmail.com>
2025-02-08 19:06:15 +0530

    Initial commit

--- operations.js
+++ operations.js
@@ -2,7 +2,7 @@ let value=3
 let negValue=-value
 console.log(negValue)
 
-console.log(2+2)
+log(2+2)
 console.log(2-2)
 console.log(2*2)
 console.log(2**2)
@@ -11,3 +11,15 @@ console.log(2**0)
 let str1="Koustav"
 let str2=" Uday"
 console.log(str1+str2)
+console.log("1"+2);
+console.log("1"+2+2);
+console.log(1+2+"1"+2);
+console.log(1+2+"1"+2+1);
+console.log(2*2+2/2-2);
+
+console.log(+true);
+
+let x=10
+++x;
+x++;
+console.log(x)
