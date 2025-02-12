64a08d9aeeb8c990f13e38e9f9d6fcca54c06f72
Koustav Biswas <bkoustav361@gmail.com>
2025-02-12 23:28:11 +0530

    Initial commit

---
+++ maths.js
@@ -0,0 +1,17 @@
+/*const number = 400
+const balance = new Number(100)
+console.log(balance)
+console.log(balance.toString().length)
+
+const num2 = 23.866
+console.log(num2.toPrecision(2))
+
+const num = 100000000
+console.log(num.toLocaleString('en-IN'))*/
+//***********MATHS//////////******* */
+console.log(Math)
+console.log(Math.abs(-4))
+console.log(Math.round(4.6))
+console.log(Math.floor(4.9))
+
+console.log(Math.random())
