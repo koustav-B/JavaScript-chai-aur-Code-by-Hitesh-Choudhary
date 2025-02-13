327d12d8b8ced60ea4b2e543b385588bcae0857e
Koustav Biswas <bkoustav361@gmail.com>
2025-02-13 20:23:31 +0530

    Initial commit

--- datetime.js
+++ datetime.js
@@ -4,3 +4,8 @@ console.log(mydate.toString())
 console.log(mydate.toLocaleString())
 console.log(typeof mydate)
 console.log(mydate.toDateString())
+
+let newdate = new Date("2025-02-14") //yymmdd
+console.log(newdate.toLocaleString());
+
+console.log(Math.floor(Date.now() / 1000))
