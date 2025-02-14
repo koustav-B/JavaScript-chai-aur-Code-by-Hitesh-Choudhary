2e7a18608b297d82c0dc4f795cc8790126e289da
Koustav Biswas <bkoustav361@gmail.com>
2025-02-14 16:09:41 +0530

    Initial commit

--- object2.js
+++ object2.js
@@ -1,2 +1,24 @@
-const tinderuser = new Object()
-console.log(tinderuser)
+const tinderuser = {}; // Declare the object
+
+tinderuser.id = "123abc"; // Correct way to add a property
+console.log(tinderuser); // { id: "123abc" }
+
+const regularuser = {
+    email: "hello.com",
+    fullname: {
+        username: {
+            firstname: "koustav",
+            lastname: "biswas"
+        }
+    }
+}
+console.log(regularuser.fullname)
+
+///combine objects
+const obj1 = { 1: "a", 2: "b" }
+const obj2 = { 3: "c", 4: "d" }
+    //const obj3 = { obj1, obj2 }
+const obj3 = Object.assign(obj1, obj2)
+const obj4 = Object.assign({}, obj1, obj2)
+console.log(obj3)
+console.log(obj4)
