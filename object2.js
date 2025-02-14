e14e7d5fc2769e32c26245c51de4ef207b437b24
Koustav Biswas <bkoustav361@gmail.com>
2025-02-14 21:02:09 +0530

    Initial commit

--- object2.js
+++ object2.js
@@ -19,6 +19,17 @@ const obj1 = { 1: "a", 2: "b" }
 const obj2 = { 3: "c", 4: "d" }
     //const obj3 = { obj1, obj2 }
 const obj3 = Object.assign(obj1, obj2)
-const obj4 = Object.assign({}, obj1, obj2)
+const obj4 = Object.assign({}, obj1, obj2) //{} is optional parameter
 console.log(obj3)
 console.log(obj4)
+
+
+const users = [{
+        id: 1,
+        email: "Koustav"
+    },
+    {
+        id: 2,
+        email: "Uday"
+    },
+]
