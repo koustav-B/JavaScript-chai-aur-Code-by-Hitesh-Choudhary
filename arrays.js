42205b9b5588da5d45502c255863c40fc3a21f67
Koustav Biswas <bkoustav361@gmail.com>
2025-02-14 13:22:17 +0530

    Initial commit

--- arrays.js
+++ arrays.js
@@ -19,3 +19,23 @@ const n1 = hero.slice(1, 3)
 const n2 = hero.splice(1, 3)
 console.log(n1)
 console.log(n2) //last range printed
+
+const l1 = [1, 2]
+const l2 = [3.4]
+const all = l1.concat(l2)
+console.log(all)
+
+//spread operator
+const ok = [...l1, ...l2]
+console.log(ok)
+
+
+
+
+console.log(Array.isArray("Hitesh"))
+console.log(Array.from("Hitesh"))
+console.log(Array.from({ name: "Koustav" })) //interesting
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1, score2, score3))
