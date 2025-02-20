ab23e0fda40729d478fcfdf514ae95d8fd9d03f9
Koustav Biswas <bkoustav361@gmail.com>
2025-02-20 20:21:35 +0530

    Initial commit

---
+++ functions.js
@@ -0,0 +1,20 @@
+/*function name() {
+    console.log("H");
+    console.log("I")
+}
+name();*/
+
+function add(num1, num2) {
+    return num1 + num2
+
+}
+//console.log(add(3, "5"));
+
+//console.log(add(3, null));
+
+
+function add(num1, num2) {
+    let res = num1 + num2
+    return res
+}
+console.log(add(2, 5))
