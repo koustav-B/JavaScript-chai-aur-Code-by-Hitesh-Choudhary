a41ca2ae5104bdb20fe297985f5d5dea9ec0307f
Koustav Biswas <bkoustav361@gmail.com>
2025-03-16 23:42:19 +0530

    Initial Commit

--- function2.js
+++ function2.js
@@ -1,4 +1,4 @@
-const user = {
+/*const user = {
     username: "koustav",
     price: 100
 }
@@ -6,4 +6,45 @@ const user = {
 function handleobject(anyobject) {
     console.log(`username is ${anyobject.username}     and price is ${anyobject.price}`);
 }
-handleobject(user)
+handleobject(user)*/
+// Object representing a person
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 30,
+    fullName: function() {
+        return `${this.firstName} ${this.lastName}`;
+    },
+    celebrateBirthday: function() {
+        this.age++;
+        console.log(`Happy Birthday, ${this.firstName}! You are now ${this.age} years old.`);
+    }
+};
+
+// Function to update person's name
+function updateName(personObj, newFirstName, newLastName) {
+    personObj.firstName = newFirstName;
+    personObj.lastName = newLastName;
+}
+
+// Function to create a new person object
+function createPerson(firstName, lastName, age) {
+    return {
+        firstName: firstName,
+        lastName: lastName,
+        age: age,
+        fullName: function() {
+            return `${this.firstName} ${this.lastName}`;
+        }
+    };
+}
+
+// Testing the object and functions
+console.log("Full Name:", person.fullName());
+person.celebrateBirthday();
+
+updateName(person, "Alice", "Smith");
+console.log("Updated Name:", person.fullName());
+
+const newPerson = createPerson("Emma", "Brown", 25);
+console.log("New Person's Name:", newPerson.fullName());
